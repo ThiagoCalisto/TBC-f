cc86e58126c447aedb5c08d858c3cc62d6484149
ThiagoCalisto <warlocalgames@gmail.com>
2022-01-31 14:55:06 -0300

    novas atts

--- components/HeadersTabs.js
+++ components/HeadersTabs.js
@@ -1,13 +1,23 @@
-import * as React from "react";
+import  React, {useState} from "react";
 import { View, Text, SafeAreaView, TouchableOpacity} from "react-native";
 import { clickProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 import { color } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 export default function HeaderTabs() {
+    const[activeTab, setActiveTab] = useState("Agendamento");
     return(
         <View style={{flexDirection: "row", alignSelf:"center"}}>
-            <HeaderButton text="Agendamento" btnColor="black" textColor="white"/>
-            <HeaderButton text="Serviços" btnColor="white" textColor="black"/>
+            <HeaderButton 
+            text="Agendamento" 
+            btnColor="black" 
+            textColor="white" 
+            activeTab={activeTab} 
+            setActiveTab={setActiveTab}/>
+            <HeaderButton text="Serviços" 
+            btnColor="white" 
+            textColor="black"
+            activeTab={activeTab} 
+            setActiveTab={setActiveTab}/>
             
         </View>
     );
@@ -19,12 +29,17 @@ const HeaderButton = (props) => (
 
     <TouchableOpacity
     style={{
-    backgroundColor: props.btnColor,
+    backgroundColor: props.activeTab == props.text ? "black" : "white",
     paddingVertical: 6,
     paddingHorizontal: 16,
     borderRadius: 40,
     }}
+    onPress={() => props.setActiveTab(props.text)}
     >
-<Text style={{color: props.textColor, fontSize: 15, fontWeight: "bold"}}>{props.text}</Text>
+<Text style={{color: props.activeTab == props.text ? "white" : "black", 
+    fontSize: 15, 
+    fontWeight: "900"}}
+    >
+    {props.text}</Text>
     </TouchableOpacity>
 );
--- screens/home.js
+++ screens/home.js
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { render } from "react-dom";
 import {View, Text, SafeAreaView} from "react-native";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 import GlobalStyle from "../components/GlobalStyle";
 import HeaderTabs from "../components/HeadersTabs";
 
 export default function Home() {
     return(
 
-    <SafeAreaView style={GlobalStyle.AndroidSafeArea}>
+    <SafeAreaView style={GlobalStyle.AndroidSafeArea, {backgroundColor: "#eee", flex: 1}}>
+        <View>
           <HeaderTabs/>
-        </SafeAreaView>
+        </View>  
+    </SafeAreaView>
 
 
     )
